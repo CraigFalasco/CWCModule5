26ca75f13e32b140e8bcc24a2c5d724de14d1f8c
CraigFalasco <craigfalasco>
2023-02-17 07:50:56 -0700

    Initial Commit

---
+++ M5CH7WebView/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  M5CH7WebView
+//
+//  Created by Craig on 2/17/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ M5CH7WebView/M5CH7WebViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  M5CH7WebViewApp.swift
+//  M5CH7WebView
+//
+//  Created by Craig on 2/17/23.
+//
+
+import SwiftUI
+
+@main
+struct M5CH7WebViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
