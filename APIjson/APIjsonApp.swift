4732cab0f11e792cbb1ea1e4bc37aea98bfafab9
CraigFalasco <craigfalasco>
2023-02-22 07:21:21 -0700

    Initial Commit

---
+++ APIjson/APIjsonApp.swift
@@ -0,0 +1,17 @@
+//
+//  APIjsonApp.swift
+//  APIjson
+//
+//  Created by Craig on 2/22/23.
+//
+
+import SwiftUI
+
+@main
+struct APIjsonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
