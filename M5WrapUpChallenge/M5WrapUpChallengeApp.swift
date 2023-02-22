499474679a35791a2de329029af5805467e50062
CraigFalasco <craigfalasco>
2023-02-22 15:11:21 -0700

    Initial Commit

---
+++ M5WrapUpChallenge/M5WrapUpChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  M5WrapUpChallengeApp.swift
+//  M5WrapUpChallenge
+//
+//  Created by Craig on 2/22/23.
+//
+
+import SwiftUI
+
+@main
+struct M5WrapUpChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
